9d236342922fd8e05b555a821e6ad4c3778e8d51
Ilya <Ilya@DESKTOP-NKVNL6R>
2023-11-24 23:43:53 +0400

    Second

---
+++ Умка _ Абакус_files/site.js
@@ -0,0 +1,19 @@
+﻿function GoToURLWithId(url, id) {
+    url = url + "?id=" + id;
+    GoToURL(url);
+}
+
+function Element_WithUrlAndId_ClickEvent(event) {
+    var url = "";
+    var id = 0;
+    var $div = $(event.currentTarget);
+
+    id = $div.data("id");
+    url = $div.data("url");
+
+    GoToURLWithId(url, id);
+}
+
+function GoToURL(url) {
+    location.href = url;
+}
